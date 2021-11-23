3120eea73c90e094f99e46a18aacccaa2e6ca9f8
wreckedCode <94932550+wreckedCode@users.noreply.github.com>
2021-11-23 20:31:38 +0000

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/uws/ac/uk/mylocation/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package uws.ac.uk.mylocation;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "uws.ac.uk.mylocation";
+  public static final String BUILD_TYPE = "debug";
+}
